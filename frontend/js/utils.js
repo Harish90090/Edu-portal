42a14ab23b37e9e70fed0442f3106190af6166d9
Harish90090 <harishachar9090@gmail.com>
2025-10-30 21:13:10 +0530

    Update utils.js

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,5 +1,5 @@
 const Utils = {
-    API_BASE_URL: 'http://localhost:5000/api',
+    API_BASE_URL: 'https://edu-portal-2.onrender.com/api',
 
     showElement: (element) => {
         if (element) element.classList.remove('hidden');
